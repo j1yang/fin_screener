0421854d7223af51056846b935ab3243b3263d66
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2024-02-22 19:39:22 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+import Body from './components/Body'
+
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Body.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
+
+const Main: React.FC = () => {
+  return (
+    <section className="text-black body-font lg:pt-20">
+      <div className="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
+        <div className="flex flex-col w-full mb-2 text-left md:text-center ">
+          <h1 className="mb-2 text-6xl font-bold tracking-tighter text-white lg:text-8xl md:text-7xl">
+            FiN4NCe Screener
+          </h1>
+          <br />
+          <p className="mx-auto  text-xl font-normal leading-relaxed text-gray-600 dark:text-gray-300 lg:w-2/3">
+            made with{" "}
+            <a href="https://react.dev/" className="underline">
+              React
+            </a>{" "}
+            and styled with Tailwind CSS
+          </p>
+        </div>
+      </div>
+      <div className="container flex flex-col items-center justify-center py-8 mx-auto rounded-lg md:p-1 p-3">
+        <div className="h-[600px] object-cover object-center w-full mb-10 border-gray-200 dark:border-gray-900 g327 border rounded-lg shadow-md">
+          
+        </div>
+      </div>
+      <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-10 mx-auto">
+            <div className="flex flex-wrap -m-4 text-center">
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={940} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Users</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={740} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Subscribes</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">
+                  <CountUp end={315} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Downloads</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+            <img
+              alt="feature"
+              className="object-cover object-center h-full w-full"
+              src="./images/placeholder.png"
+            />
+          </div>
+          <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+            <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex-grow">
+                <h2 className="text-white text-2xl title-font font-medium mb-3">
+                  Free
+                </h2>
+                <p className="leading-relaxed text-lg">
+                  All of our templates are 100% free forever.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex-grow">
+                <h2 className="text-white text-2xl title-font font-medium mb-3">
+                  Responsive
+                </h2>
+                <p className="leading-relaxed text-lg">
+                  All our templates come with full responsiveness straight out
+                  of the box.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex-grow">
+                <h2 className="text-white text-2xl title-font font-medium mb-3">
+                  SEO Friendly
+                </h2>
+                <p className="leading-relaxed text-lg">
+                  Our templates have the best SEO practices ensuring you get to
+                  the top.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex-grow">
+                <h2 className="text-white text-2xl title-font font-medium mb-3">
+                  Fast
+                </h2>
+                <p className="leading-relaxed text-lg">
+                  The fastest websites you can get.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex-grow">
+                <h2 className="text-white text-2xl title-font font-medium mb-3">
+                  Google Analytics Supported
+                </h2>
+                <p className="leading-relaxed text-lg">
+                  All our templates come with full support for Google Analytics.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 mx-auto">
+          <div className="text-center mb-20">
+            <h2 className="sm:text-5xl font-medium title-font text-white mb-4">
+              Our Team
+            </h2>
+            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+              Here is our company
+            </p>
+            <div className="flex mt-6 justify-center">
+              <div className="w-16 h-1 rounded-full bg-white inline-flex"></div>
+            </div>
+            <div className="container px-5 py-16 mx-auto">
+              <div className="flex flex-wrap -m-4">
+                {/* Teams */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Main;
